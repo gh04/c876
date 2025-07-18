5059064c503aa4a893156720ac2fa9b99fc2262b
Gerardo Hernandez <gahernandez02@gmail.com>
2025-07-18 11:31:24 -0700

    initial commit

--- C867/main.cpp
+++ C867/main.cpp
@@ -6,9 +6,18 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    while (argc < 2) {
+        cout << "incorrect argument\n";
+        return 1;
+    }
+    
+    cout << argv[1] << " " << argv[2] << endl;
+    
+    
+//    std::cout << "Hello, World!\n";
     return 0;
 }
