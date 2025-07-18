ee75d5f447adb12e62b0e8aad403f8275af03d60
Gerardo Hernandez <gahernandez02@gmail.com>
2025-07-18 11:51:58 -0700

    added new file

--- C867/practice/arrays and vectors/vectorNth.cpp
+++ C867/practice/arrays and vectors/vectorNth.cpp
@@ -8,7 +8,7 @@ int main() {
 
    oldestPeople.at(0) = 122; // Died 1997 in France
    oldestPeople.at(1) = 119; // Died 1999 in U.S.
-   oldestPeople.at(2) = 117; // Died 1993 in U.S. 
+   oldestPeople.at(2) = 117; // Died 1993 in U.S.
    oldestPeople.at(3) = 117; // Died 1998 in Canada
    oldestPeople.at(4) = 116; // Died 2006 in Ecuador
 
