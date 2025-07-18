b02a8d19f356510f6e3829b5f9107d3f4e43b19d
Gerardo Hernandez <gahernandez02@gmail.com>
2025-07-18 14:11:49 -0700

    Added solution to vectorLoopSalary.

--- C867/practice/arrays and vectors/vectorLoopSalary.cpp
+++ C867/practice/arrays and vectors/vectorLoopSalary.cpp
@@ -2,6 +2,20 @@
 #include <vector>
 using namespace std;
 
+/*
+    Integer vecVals is read from input as the number of elements 
+    in vector averageSalary. Given the integer vector averageSalary, 
+    write a for loop that assigns sumBonus with the total bonus. Each 
+    salary value above 300 contains a bonus equal to the salary value 
+    minus 300.
+
+    Ex: If the input is:
+    3
+    49 352 366
+    
+    then the output is:
+    Bonus sum: 118
+*/
 int main() {
    vector<int> averageSalary;
    int vecVals;
@@ -18,7 +32,11 @@ int main() {
 
    sumBonus = 0;
 
-   /* Your code goes here */
+   for (i = 0; i < averageSalary.size(); ++i) {
+    if (averageSalary.at(i) > 300) {
+        sumBonus += averageSalary.at(i) - 300;
+    }
+   }
 
    cout << "Bonus sum: " << sumBonus << endl;
    
