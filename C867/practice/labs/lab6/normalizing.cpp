20d22c194ee8b379c5682cfa01a687b4db660b11
Gerardo Hernandez <gahernandez02@gmail.com>
2025-07-19 08:00:17 -0700

    Added solution to lab 6.21

---
+++ C867/practice/labs/lab6/normalizing.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+vector<double> floatList;
+int inputNumCount; // n of elements in vector
+double inputValues; // element values of vector
+double maxValue; // max element value
+int i;
+unsigned j;
+
+void getNumCount();
+void populateList(int nElements);
+int getMax(vector<double> list);
+void normalize(vector<double> list, double max);
+
+int main() {
+
+    getNumCount(); // Ask for n of elements
+    populateList(inputNumCount); // Populate vector with n elements
+    normalize(floatList, getMax(floatList)); // divide each element value by max and ouput the result
+
+    cout << endl;
+
+    return 0;
+}
+
+void getNumCount() {
+    do {
+        cin >> inputNumCount;
+    } while (inputNumCount <= 0);
+}
+
+void populateList(int nElements) {
+    for (i = 0; i < nElements; ++i) {
+        cin >> inputValues;
+        floatList.push_back(inputValues);
+    }
+}
+
+int getMax(vector<double> list) {
+    maxValue = list[0];
+
+    for (j = 0; j < list.size(); ++j) {
+        if (list.at(j) > maxValue) {
+            maxValue = list.at(j);
+        }
+    }
+    return maxValue;
+}
+
+void normalize(vector<double> list, double max) {
+    for (j = 0; j < list.size(); ++j) {
+        cout << fixed 
+             << setprecision(2) 
+             << list.at(j) / max
+             << " ";
+    }
+} 
