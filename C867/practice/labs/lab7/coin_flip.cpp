33988fa84920aaa57130cba3674929cdbcde7908
Gerardo Hernandez <gahernandez02@gmail.com>
2025-07-20 14:10:43 -0700

    Added error handling in GetInput

--- C867/practice/labs/lab7/coin_flip.cpp
+++ C867/practice/labs/lab7/coin_flip.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cstdlib>
-#include <time.h>
+#include <ctime>
 using namespace std;
 
 int GetInput();
@@ -10,7 +10,7 @@ void PrintResult(int nTimes);
 
 
 int main() {
-    srand(2);
+    srand(2); // Setting the seed requirement that is provided
     PrintResult(GetInput());
 
     return 0;
@@ -18,12 +18,15 @@ int main() {
 
 int GetInput() {
     int i;
-    cin >> i;
+    if (!(cin >> i)) {
+        cerr << "Invalid input.\n";
+        exit(1);
+    }
     return i;
 }
 
 string CoinFlip() {
-   return (RandN == 0) ? "Tails" : "Heads";
+   return (RandN() == 0) ? "Tails" : "Heads";
 }
 
 int RandN() {
@@ -31,9 +34,7 @@ int RandN() {
 }
 
 void PrintResult(int nTimes) {
-    int i;
-
-    for (i = 0; i < nTimes; ++i) {
+    for (int i = 0; i < nTimes; ++i) {
         cout << CoinFlip() << endl;
     }
 }
