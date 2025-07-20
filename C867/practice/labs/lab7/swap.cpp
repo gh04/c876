e72bc0a682d0c6201eed8c36decd26c95636258a
Gerardo Hernandez <gahernandez02@gmail.com>
2025-07-20 12:47:19 -0700

    Implemented SwapValues method and finished solution

--- C867/practice/labs/lab7/swap.cpp
+++ C867/practice/labs/lab7/swap.cpp
@@ -58,5 +58,19 @@ bool CheckVectorSize(const vector<int>& vectorArr) {
 }
 
 void SwapValues(int& userVal1, int& userVal2, int& userVal3, int& userVal4) {
+    int temp1 = userVal1;
+    int temp2 = userVal3;
+    userVal1 = userVal2;
+    userVal2 = temp1;
+    userVal3 = userVal4;
+    userVal4 = temp2;
+
+    cout << userVal1 
+         << " " 
+         << userVal2 
+         << " " 
+         << userVal3 
+         << " " 
+         << userVal4 << endl;
 
 }
