829c8fdda1c47ec04e7f8eb0fa1e5768a11daee2
Gerardo Hernandez <gahernandez02@gmail.com>
2025-07-24 16:50:14 -0700

    Implemented header file FoodItem

---
+++ C867/practice/labs/lab8/Nutritional/FoodItem.h
@@ -0,0 +1,27 @@
+#ifndef FOODITEM_H
+#define FOODITEM_H
+
+#include <string>
+using namespace std;
+
+class FoodItem {
+    public:
+        FoodItem();
+        FoodItem(string name, double fat, double carbs, double protein);
+
+        // Getters
+        string GetName() const;
+        double GetFat() const;
+        double GetCarbs() const;
+        double GetProtein() const;
+        double GetCalories(double numServings) const;
+        
+        // Other
+    private:
+        string name;
+        double fat;
+        double carbs;
+        double protien;
+};
+
+#endif
