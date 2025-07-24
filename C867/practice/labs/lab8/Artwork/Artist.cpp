03b6ac1829151ef3afe71edd917fcab9818eb959
Gerardo Hernandez <gahernandez02@gmail.com>
2025-07-24 08:13:20 -0700

    Implemented Artist.cpp

---
+++ C867/practice/labs/lab8/Artwork/Artist.cpp
@@ -0,0 +1,31 @@
+#include "Artist.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Default Constructor
+Artist::Artist() : artistName("unknown"), birthYear(-1), deathYear(-1) {};
+
+// Overloaded Constructor
+Artist::Artist(string artistName, int birthYear, int deathYear) {
+this->artistName = artistName;
+this->birthYear = birthYear;
+this->deathYear = deathYear;
+}
+
+string Artist::GetName() const { return artistName; }
+
+int Artist::GetBirthYear() const { return birthYear; }
+
+int Artist::GetDeathYear() const { return deathYear; }
+
+void Artist::PrintInfo() const {
+    if (birthYear > -1 && deathYear > -1) {
+        cout << "(" << birthYear << " to " << deathYear << ")";
+    } else if (birthYear > -1 && deathYear < 0) {
+        cout << "(" << birthYear << " to present)";
+    } else {
+        cout << "(Unkown)";
+    }
+    cout << endl;
+}
